4bc8d17b44c57dc24b193ac3053194d5806e9609
elgokar231m-hub <elgokar231m@gmail.com>
2026-04-20 22:53:01 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "images.search.yahoo.com" },
+      { protocol: "https", hostname: "**" },
+    ],
+  },
+};
+
+export default nextConfig;
